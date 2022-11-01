170ab13e03045b2690dd482a07c0363ad4ef3662
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-11-01 13:35:26 +0000

    Update randwalk.py

--- pages/randwalk.py
+++ pages/randwalk.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import streamlit as st
 
 # Fixing random state for reproducibility
 np.random.seed(19680801)
@@ -42,4 +43,4 @@ ax.set(zlim3d=(0, 1), zlabel='Z')
 ani = animation.FuncAnimation(
     fig, update_lines, num_steps, fargs=(walks, lines), interval=100)
 
-st.pyplot(ani)
+st.pyplot(fig)

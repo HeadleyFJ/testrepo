3632f429d406fe6088c8ed02042ad4a2fffe25c2
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-11-29 12:09:10 +0000

    Create wigettest.py

---
+++ pages/wigettest.py
@@ -0,0 +1,11 @@
+import rebound
+import streamlit as st
+
+
+sim = rebound.Simulation()
+sim.add(m=1) # add a star
+for i in range(10):
+    sim.add(m=1e-3,a=0.4+0.1*i,inc=0.03*i,omega=5.*i) # Jupiter mass planets on close orbits
+sim.move_to_com() # Move to the centre of mass frame
+
+sim.getWidget()

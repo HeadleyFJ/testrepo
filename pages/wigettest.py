11fad3eff536b15fa6838bd2588bc9a5d4c4f323
HeadleyFJ <115649019+HeadleyFJ@users.noreply.github.com>
2022-11-29 12:12:00 +0000

    Update wigettest.py

--- pages/wigettest.py
+++ pages/wigettest.py
@@ -9,6 +9,6 @@ for i in range(10):
     sim.add(m=1e-3,a=0.4+0.1*i,inc=0.03*i,omega=5.*i) # Jupiter mass planets on close orbits
 sim.move_to_com() # Move to the centre of mass frame
 
-st.write(rebound.OrbitPlot(sim))
+st.image(rebound.OrbitPlot(sim))
 
 sim.getWidget()
